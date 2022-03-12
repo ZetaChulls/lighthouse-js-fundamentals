b8d42e75d5cf85bee8991ac482914488cf55de59
Charles Ngo <charlesngo37@gmail.com>
2022-03-12 15:29:20 -0800

    Function to determine if a number is odd

---
+++ odd.js
@@ -0,0 +1,9 @@
+// Function to determine if an input is an odd value. Will return True if the input is odd and False if even
+
+function isOdd(num) {
+  return num % 2 === 1;
+}
+
+// Testing statements
+console.log("3 is odd: " + isOdd(3));
+console.log("8 is odd: " + isOdd(8));
