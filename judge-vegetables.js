2c2d67afcd760df325037d324930095024d1772a
Charles Ngo <charlesngo37@gmail.com>
2022-03-15 22:54:36 -0700

    function to iterate over objects

---
+++ judge-vegetables.js
@@ -0,0 +1,35 @@
+const judgeVegetable = function (vegetables, metric) {
+  let winner = ''
+  let winningMetric = 0;
+  for (let i = 0; i < vegetables.length; i++) {
+    if (vegetables[i][metric] > winningMetric) {
+      winningMetric = vegetables[i][metric];
+      winner = vegetables[i]['submitter'];
+    }
+  }
+  return winner;
+}
+
+const vegetables = [
+  {
+    submitter: 'Old Man Franklin',
+    redness: 10,
+    plumpness: 5
+  },
+  {
+    submitter: 'Sally Tomato-Grower',
+    redness: 2,
+    plumpness: 8
+  },
+  {
+    submitter: 'Hamid Hamidson',
+    redness: 4,
+    plumpness: 3
+  }
+]
+
+const metric = 'redness'
+
+judgeVegetable(vegetables, metric)
+
+console.log(judgeVegetable(vegetables, metric))
